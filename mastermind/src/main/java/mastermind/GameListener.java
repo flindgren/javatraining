5fed9871d11f47f02886e22adca7f5c4458fd6d0
Fredrik <fredrik@lindgren.name>
2013-02-21 01:31:49 +0100

    Cleanup of experimental stuff

--- mastermind/src/main/java/mastermind/GameListener.java
+++ mastermind/src/main/java/mastermind/GameListener.java
@@ -4,6 +4,8 @@ package mastermind;
  * Handles game domain events
  */
 public interface GameListener {
+
+    void gameStarted();
     void guessProcessed(Object guess, Object key);
 
     void gameCompleted(boolean solved, int pointsScored);
--- mastermind/src/test/resources/game.feature
+++
@@ -1,9 +0,0 @@
-Feature: Game Flow
-
-  Scenario: Correct guess
-    Given a secret ABCD
-    When guessing ABCD
-    Then The code was solved
-
-
-
